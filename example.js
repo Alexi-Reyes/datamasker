1de8d440d435ed899818f33d2a9f4f742f82b832
Alexi Reyes <alexireyes6796@gmail.com>
2025-07-07 13:51:07 +0200

    added lin and format to github ci

--- example.js
+++ example.js
@@ -1,6 +1,6 @@
 const DataMasker = require('./src/index');
 
-console.log("Generated Name:", DataMasker.person.fullName());
-console.log("Generated First Name:", DataMasker.person.firstName());
-console.log("Generated Last Name:", DataMasker.person.lastName());
-console.log("Generated Mail:", DataMasker.mail.randomEmail());
+console.log('Generated Name:', DataMasker.person.fullName());
+console.log('Generated First Name:', DataMasker.person.firstName());
+console.log('Generated Last Name:', DataMasker.person.lastName());
+console.log('Generated Mail:', DataMasker.mail.randomEmail());
